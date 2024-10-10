53c6559b9565c17fd6a9223505986b7c0ac0f92d
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-10-10 20:31:26 +0400

    UI update - LoginScreen

--- lib/Authentication/loginScreen.dart
+++ lib/Authentication/loginScreen.dart
@@ -1,26 +1,38 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Get the screen size
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.all(10),
+      body: SingleChildScrollView(
+        child: Container(
+          height: size.height, // Set the container height to full screen height
+          padding: EdgeInsets.all(20),
+          child: Center(
+            // Wrap the Column with Center
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Center(
-                  child: Text(
-                    'KW',
-                    style: TextStyle(fontSize: 40),
-                  ),
-                )
+                Text(
+                  'KWYE',
+                  style: GoogleFonts.poppins(
+                      fontSize: 60, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'know what you eat',
+                  style: GoogleFonts.poppins(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.grey),
+                ),
               ],
             ),
           ),
