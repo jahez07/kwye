bb0169e133847ca745d6bfa31d254f373a8fa1da
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-10-12 17:07:12 +0400

    Clean Code update

--- lib/Authentication/loginScreen.dart
+++ lib/Authentication/loginScreen.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:get/route_manager.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kwye/Screens/choices.dart';
