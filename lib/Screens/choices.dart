1438638f88b72583abfe9b0fef5503891523f7bc
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-10-15 18:18:20 +0400

    Logout - TextButton

--- lib/Screens/choices.dart
+++ lib/Screens/choices.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:kwye/Repository/Authentication/authentication_repository.dart';
 
 class ChoicesScreen extends StatelessWidget {
   const ChoicesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
@@ -16,41 +18,57 @@ class ChoicesScreen extends StatelessWidget {
       ),
       backgroundColor: Colors.white,
       body: Center(
-        child: Row(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            GestureDetector(
-              onTap: () {},
-              child: Container(
-                padding: const EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade100,
-                  borderRadius: BorderRadius.circular(15),
-                  border: Border.all(width: 1, color: Colors.grey.shade300),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade100,
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(width: 1, color: Colors.grey.shade300),
+                    ),
+                    child: const Text(
+                      'Packed Food',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
                 ),
+                const SizedBox(
+                  width: 10,
+                ),
+                GestureDetector(
+                  child: Container(
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade100,
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(width: 1, color: Colors.grey.shade300),
+                    ),
+                    child: const Text(
+                      'Cooked Food',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: size.height * 0.01,
+            ),
+            TextButton(
+                onPressed: () {
+                  AuthenticationRepository.instance.logout();
+                },
                 child: const Text(
-                  'Packed Food',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            GestureDetector(
-              child: Container(
-                padding: const EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade100,
-                  borderRadius: BorderRadius.circular(15),
-                  border: Border.all(width: 1, color: Colors.grey.shade300),
-                ),
-                child: const Text(
-                  'Cooked Food',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ),
+                  'SignOut',
+                  style: TextStyle(color: Colors.blueAccent, fontSize: 15),
+                ))
           ],
         ),
       ),
