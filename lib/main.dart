ddb1afb2f7b502491a29e7aaabda816f8bffcddd
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-10-15 17:15:52 +0400

    Calling the AuthenticationRepository in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,17 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/get_instance.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:kwye/Authentication/loginScreen.dart';
+import 'package:kwye/Repository/Authentication/authentication_repository.dart';
 import 'package:kwye/firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((value) => Get.put(AuthenticationRepository()));
   runApp(const MyApp());
 }
 
