be02bb6389cfc8ba8bbe3ff49fc98f500db02863
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-10-15 16:32:00 +0400

    Authentication Repository

---
+++ lib/Repository/Authentication/authentication_repository.dart
@@ -0,0 +1,7 @@
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/get_instance.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+class AuthenticationRepository extends GetxController {
+  static AuthenticationRepository get instance => Get.find();
+}
