ba3293d994bc48823d3b933a91c6827169ea3a02
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-10-16 07:04:48 +0400

    GettingStarted screen

--- lib/Screens/getting_started.dart
+++ lib/Screens/getting_started.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:kwye/Authentication/loginScreen.dart';
 
 class GettingStarted extends StatelessWidget {
   const GettingStarted({super.key});
@@ -11,9 +15,24 @@ class GettingStarted extends StatelessWidget {
       padding: const EdgeInsets.all(10),
       child: Column(
         children: [
-          Image(
-            image: AssetImage(''),
-          )
+          GestureDetector(
+            onTap: () => Get.to(() => const LoginScreen()),
+            child: Container(
+              height: 60,
+              width: 150,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(width: 1, color: Colors.grey)),
+              child: Center(
+                child: Text(
+                  'Get Started',
+                  style: GoogleFonts.poppins(
+                      fontSize: 25, fontWeight: FontWeight.w500),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
