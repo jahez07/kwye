9ef42c5f698f1bfff183aac136cee09977286112
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-10-17 19:12:59 +0400

    GettingStarted UI update

--- lib/Screens/getting_started.dart
+++ lib/Screens/getting_started.dart
@@ -10,36 +10,42 @@ class GettingStarted extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        padding: const EdgeInsets.all(10),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GestureDetector(
-                onTap: () => Get.to(() => const LoginScreen()),
-                child: Container(
-                  height: 60,
-                  width: 200,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-                      border: Border.all(width: 1, color: Colors.grey)),
-                  child: Center(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Get Started',
-                        style: GoogleFonts.poppins(
-                            fontSize: 25, fontWeight: FontWeight.w500),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  GestureDetector(
+                    onTap: () => Get.to(() => const LoginScreen()),
+                    child: Container(
+                      height: 60,
+                      width: size.width * 1,
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(width: 1, color: Colors.grey)),
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Get Started',
+                            style: GoogleFonts.poppins(
+                                fontSize: 25, fontWeight: FontWeight.w500),
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
