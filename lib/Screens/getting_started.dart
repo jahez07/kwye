be81b5c496f3d3ebb2e1231b862b67ac322eafc5
jahez07 <61622091+jahez07@users.noreply.github.com>
2024-10-18 09:47:44 +0400

    LinearGradient update

--- lib/Screens/getting_started.dart
+++ lib/Screens/getting_started.dart
@@ -12,76 +12,92 @@ class GettingStarted extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(right: 15.0, left: 15.0),
-          child: Container(
-            padding: const EdgeInsets.all(10),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  RichText(
-                    text: TextSpan(children: [
-                      TextSpan(
-                        text: 'Knowing\n',
-                        style: GoogleFonts.poppins(
-                          fontSize: 60,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.black,
-                          height: 1.1, // Reduced line height for the first line
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.0, 0.3, 0.5, 1.0],
+            colors: [
+              Colors.grey.shade800, // Slightly darker purple at the bottom
+              Colors.grey.shade400, // Light purple starts before halfway
+              Colors.grey.shade50, // Very light blue
+              Colors.white, // Start with white at the top
+            ],
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(right: 15.0, left: 15.0),
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      text: TextSpan(children: [
+                        TextSpan(
+                          text: 'Knowing\n',
+                          style: GoogleFonts.poppins(
+                            fontSize: 60,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black,
+                            height: 1.1,
+                          ),
                         ),
-                      ),
-                      WidgetSpan(
-                          child: SizedBox(
-                        height: size.height * 0,
-                      )),
-                      TextSpan(
-                        text: 'What You \nAre Eating',
-                        style: GoogleFonts.poppins(
-                          fontSize: 60,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.grey.shade400,
-                          height: 1.1, // Reduced line height for the first line
+                        WidgetSpan(
+                            child: SizedBox(
+                          height: size.height * 0,
+                        )),
+                        TextSpan(
+                          text: 'What You \nAre Eating',
+                          style: GoogleFonts.poppins(
+                            fontSize: 60,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.grey.shade400,
+                            height: 1.1,
+                          ),
                         ),
-                      ),
-                    ]),
-                  ),
-                  SizedBox(
-                    height: size.height * 0.02,
-                  ),
-                  Text(
-                    'Lorem ipsum odor amet, consectetuer adipiscing elit. Tempus tortor dictum euismod',
-                    style: GoogleFonts.poppins(
-                        fontSize: 15, color: Colors.grey.shade500),
-                  ),
-                  SizedBox(
-                    height: size.height * 0.05,
-                  ),
-                  GestureDetector(
-                    onTap: () => Get.to(() => const LoginScreen()),
-                    child: Container(
-                      height: 60,
-                      width: size.width * 1,
-                      decoration: BoxDecoration(
-                          color: Colors.blueAccent,
-                          borderRadius: BorderRadius.circular(30),
-                          border: Border.all(width: 1, color: Colors.grey)),
-                      child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            'Get Started',
-                            style: GoogleFonts.poppins(
-                                fontSize: 25, fontWeight: FontWeight.w500),
+                      ]),
+                    ),
+                    SizedBox(
+                      height: size.height * 0.02,
+                    ),
+                    Text(
+                      'Lorem ipsum odor amet, consectetuer adipiscing elit. Tempus tortor dictum euismod',
+                      style: GoogleFonts.poppins(
+                          fontSize: 15, color: Colors.grey.shade500),
+                    ),
+                    SizedBox(
+                      height: size.height * 0.05,
+                    ),
+                    GestureDetector(
+                      onTap: () => Get.to(() => const LoginScreen()),
+                      child: Container(
+                        height: 60,
+                        width: size.width * 1,
+                        decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(30),
+                            border: Border.all(width: 1, color: Colors.grey)),
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Get Started',
+                              style: GoogleFonts.poppins(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
